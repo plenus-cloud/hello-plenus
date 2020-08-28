6550816d9dc6021055beffd9f988903199737d7b
Massimiliano Ferrero <m.ferrero@cognitio.it>
2020-08-28 14:10:55 +0200

    Fix delete script

--- delete.sh
+++ delete.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
 kubectl delete -f yaml/
-kubectl create secret generic hello-css --from-file=css/ --dry-run=client -o yaml | kubectl delete -f -
-kubectl create secret generic hello-images --from-file=images/ --dry-run=client -o yaml | kubectl delete -f -
